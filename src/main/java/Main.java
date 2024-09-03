3adbde0ca5281801348fddc8704e2e401ba0acdb
Gabrielle Dias <169068323+GABRIELLEDIAS19@users.noreply.github.com>
2024-09-03 02:20:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Ola! Digite sua senha");
+
+    int senha = sc.nextInt();
+
+    while (senha != 2022) {
+      System.out.println("Senha incorreta");
+      System.out.println("Digite novamente");
+      senha = sc.nextInt();
+    }
+    
+    if(senha == 2022) {
+      System.out.println("Acesso permitido");
+    }
+
+
+    sc.close();
+    
+  }
+
+  
+}
